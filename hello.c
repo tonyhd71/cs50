d26ff5af43be647bce670f7651e2b86254a3e04e
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-05-12 17:45:56 -0500

    still week 1

--- hello.c
+++ hello.c
@@ -6,5 +6,5 @@ int main(void)
     string first = get_string("What is your first name? ");
     string last = get_string("What is your last name? ");
 
-    printf("hello, %s\n", first);
+    printf("I got 100%%\n", first);
 }

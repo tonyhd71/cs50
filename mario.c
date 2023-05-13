544ca110862ef23f6cdee43e419f263180d2955f
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-05-12 23:15:34 -0500

    defining functions

--- mario.c
+++ mario.c
@@ -1,20 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+
 int main(void)
 {
+    // Get size of gris
+    int n = get_size();
+
+    // Print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
     int n;
-    do 
+    do
     {
         n = get_int("Size: ");
     }
-    while(n < 1);
-    
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j =0; j < n; j++) 
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    while (n < 1);
+    return n;
 }
+void print_grid(int)

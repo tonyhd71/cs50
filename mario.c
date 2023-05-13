8cb5f4de017afbe13480eb4dbca36b08c1df943b
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-05-12 22:59:56 -0500

    do while loops

--- mario.c
+++ mario.c
@@ -1,15 +1,20 @@
+#include <cs50.h>
 #include <stdio.h>
-
 int main(void)
 {
-    for (int i = 0; i < 3; i++) 
+    int n;
+    do 
     {
-        for (int j =0; j < 3; j++) 
+        n = get_int("Size: ");
+    }
+    while(n < 1);
+    
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j =0; j < n; j++) 
         {
             printf("#");
         }
         printf("\n");
-
     }
-    //printf("\n");
 }

f68bf7207053b0c543df0a9e715072c18e4770bf
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-05-13 19:36:55 -0500

    starting cents lab

---
+++ cash.js
@@ -0,0 +1,20 @@
+let cents;;
+let pennies;
+let nickels;
+let dimes;
+let quarters;
+function getCents() {
+
+}
+function calculatePennies() {
+
+}
+function calculateNickels() {
+
+}
+function calculateDimes() {
+
+}
+function calculateQuarters()) {
+
+}

36f10f3d2571803c1b6179e98af9ea3d3cabddb2
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-05-15 14:04:08 -0500

    week 1 credit problem

---
+++ credit.js
@@ -0,0 +1,7 @@
+function multiplyEveryOther(number, strOfNumber, numberLength) {
+    strOfNumber = number.toString();
+    numberLength = strOfNumber.length -1;
+    alert(numberLength);
+   //alert(strOfNumber.length);
+}
+multiplyEveryOther(4003600000000014);
